0d6635c4d58ddc720a9856bce465577b05e00eef
lee-jung-hee <zhongxi710@gmail.com>
2025-07-02 16:03:24 +0900

    Make todo list using React

--- src/App.jsx
+++ src/App.jsx
@@ -2,40 +2,59 @@ import { useState } from "react";
 import "./App.css";
 
 function App() {
+  function formatDate(dateStr) {
+    const date = new Date(dateStr);
+    return new Intl.DateTimeFormat("ko-KR", {
+      year: "numeric",
+      month: "numeric",
+      day: "numeric",
+      hour12: false,
+    }).format(date);
+  }
+
   const [todoList, setTodoList] = useState([
-    { id: 0, content: "123" },
-    { id: 1, content: "코딩 공부하기" },
-    { id: 2, content: "잠 자기" },
+    {
+      id: 0,
+      content: "Have meal",
+      time: formatDate("2025-06-03T08:20:00"),
+      checked: false,
+    },
+    {
+      id: 1,
+      content: "Drink coffee",
+      time: formatDate("2025-06-16T15:37:00"),
+      checked: false,
+    },
   ]);
-
-  return (
-    <>
-      <TodoList todoList={todoList} setTodoList={setTodoList} />
-      <hr />
-      <TodoInput todoList={todoList} setTodoList={setTodoList} />
-    </>
-  );
-}
-
-function TodoInput({ todoList, setTodoList }) {
   const [inputValue, setInputValue] = useState("");
 
+  const handleList = () => {
+    setTodoList([
+      ...todoList,
+      {
+        id:
+          todoList.length > 0
+            ? Math.max(...todoList.map((todo) => todo.id)) + 1
+            : 0,
+        content: inputValue,
+        time: formatDate(new Date()),
+        checked: false,
+      },
+    ]);
+  };
+
   return (
     <>
-      <input
-        value={inputValue}
-        onChange={(event) => setInputValue(event.target.value)}
-      />
-      <button
-        onClick={() => {
-          const newTodo = { id: Number(new Date()), content: inputValue };
-          const newTodoList = [...todoList, newTodo];
-          setTodoList(newTodoList);
-          setInputValue("");
-        }}
-      >
-        추가하기
-      </button>
+      <main>
+        <h1>✅ My Secret Note 🗒️</h1>
+        <TodoList todoList={todoList} setTodoList={setTodoList} />
+        <hr />
+        <TodoInput
+          inputValue={inputValue}
+          handleList={handleList}
+          setInputValue={setInputValue}
+        />
+      </main>
     </>
   );
 }
@@ -43,43 +62,108 @@ function TodoInput({ todoList, setTodoList }) {
 function TodoList({ todoList, setTodoList }) {
   return (
     <ul>
-      {todoList.map((todo) => (
-        <Todo key={todo.id} todo={todo} setTodoList={setTodoList} />
-      ))}
+      {todoList.length === 0 ? (
+        <span>Make your list</span>
+      ) : (
+        todoList.map((todo) => (
+          <Todo key={todo.id} todo={todo} setTodoList={setTodoList} />
+        ))
+      )}
     </ul>
   );
 }
 
-function Todo({ todo, setTodoList }) {
-  const [inputValue, setInputValue] = useState("");
+function TodoInput({ inputValue, handleList, setInputValue }) {
+  const handleInputValue = (e) => {
+    setInputValue(e.target.value);
+  };
+
   return (
-    <li>
-      {todo.content}
-      <input
-        value={inputValue}
-        onChange={(event) => setInputValue(event.target.value)}
-      />
+    <>
+      <input type="text" value={inputValue} onChange={handleInputValue} />
       <button
         onClick={() => {
-          setTodoList((prev) =>
-            prev.map((el) =>
-              el.id === todo.id ? { ...el, content: inputValue } : el
-            )
-          );
+          handleList();
+          setInputValue("");
         }}
       >
-        수정
+        Add List
       </button>
-      <button
-        onClick={() => {
-          setTodoList((prev) => {
-            return prev.filter((el) => el.id !== todo.id);
-          });
-        }}
-      >
-        삭제
-      </button>
-    </li>
+    </>
+  );
+}
+
+function Todo({ todo, setTodoList }) {
+  const [inputValue, setInputValue] = useState(todo.content);
+  const [modify, setModify] = useState(false);
+
+  const handleDelete = () => {
+    setTodoList((prev) => prev.filter((list) => list.id !== todo.id));
+  };
+
+  const handleCheck = () => {
+    setTodoList((prev) =>
+      prev.map((item) =>
+        item.id === todo.id ? { ...item, checked: !item.checked } : item
+      )
+    );
+  };
+
+  const handleModify = (e) => {
+    setInputValue(e.target.value);
+  };
+
+  const handleChangeBtn = () => {
+    setModify((prev) => !prev);
+
+    setTodoList((prev) =>
+      prev.map((item) =>
+        item.id === todo.id ? { ...item, content: inputValue } : item
+      )
+    );
+  };
+
+  const liStyle = {
+    textDecoration: todo.checked ? "line-through" : "none",
+    color: todo.checked ? "gray" : "black",
+  };
+
+  const spanStyle = {
+    display: modify ? "none" : "inline",
+    margin: 10,
+  };
+
+  const modifyStyle = {
+    display: modify ? "inline" : "none",
+    marginLeft: 10,
+    marginRight: 10,
+  };
+  console.log(todo.content);
+
+  return (
+    <>
+      <li style={liStyle}>
+        <span>
+          <input
+            type="checkbox"
+            checked={todo.checked || false}
+            onChange={handleCheck}
+          />
+          To do :
+          <input
+            onChange={handleModify}
+            style={modifyStyle}
+            value={inputValue}
+          />
+          <span style={spanStyle}>{todo.content} </span>
+        </span>
+        Time : {todo.time}
+        <span>
+          <button onClick={handleChangeBtn}>Modify</button>
+          <button onClick={handleDelete}>❌</button>
+        </span>
+      </li>
+    </>
   );
 }
 
